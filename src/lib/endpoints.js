c0985eeedd2110ffc186b2ed846d080426558c59
safanador <sergioafanador102@gmail.com>
2025-04-26 00:06:05 -0500

    Production

--- src/lib/endpoints.js
+++ src/lib/endpoints.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = /*process.env.NEXT_PUBLIC_API_BASE_URL || */'http://localhost:3000/api/v1';
+const API_BASE_URL = /*process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api/v1'*/'https://api.cloudnel.com/api/v1';
 
 const endpoints = {
   
